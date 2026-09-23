60f0644fb208b9959a713b69251fde2b0e8481fa
paxnurlana <paxnurlana@gmail.com>
2026-09-23 13:29:51 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -18,16 +18,4 @@
     revealItems.forEach((item) => item.classList.add('is-visible'));
   }
 
-  const videoShell = document.querySelector('[data-video-id]');
-  const videoButton = videoShell?.querySelector('.video-poster');
-
-  videoButton?.addEventListener('click', () => {
-    const videoId = videoShell.dataset.videoId;
-    const iframe = document.createElement('iframe');
-    iframe.src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0`;
-    iframe.title = 'Видео о пути трейдера и системе анализа золота';
-    iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-    iframe.allowFullscreen = true;
-    videoShell.replaceChildren(iframe);
-  }, { once: true });
 })();
